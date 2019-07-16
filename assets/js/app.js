a37485d6500b9fa0845c446ef978a8168de4b9a5
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-07-16 01:34:39 -0400

    navbar with links. ajax to get all divs based on nav-link text.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,46 @@
-var queryItem = "Hello";
+var queriesArray = []
 
-let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=GKwA57i0nyuYd3slVSsZEWet5HMrfuD3&q=" + queryItem + "&limit=10&offset=0&rating=PG-13&lang=en"
+const createQueryURL = (queryItem) => {
+    return "https://api.giphy.com/v1/gifs/search?api_key=GKwA57i0nyuYd3slVSsZEWet5HMrfuD3&q=" + queryItem + "&limit=10&offset=0&rating=PG-13&lang=en";
+}
 
-$.ajax({
-        url: queryURL,
+
+
+const getGifs = (queryItem) => {
+    $.ajax({
+        url: createQueryURL(queryItem),
         method: 'get'
     })
-    .then(res => {
-        return res.data;
-    })
-    .then(data => {
-        for (gif in data) {
-            console.log(gif);
-        }
-    });
+        .then(res => {
+            for (let gifObject of res.data) {
+                var imgElement = $('<img>');
+                imgElement.attr('src', gifObject.images.downsized.url);
+                imgElement.addClass('col-lg-3 col-md-4 my-3 rounded');
+                $('#gifContainer').append(imgElement);
+
+            }
+
+        });
+}
+
+$('.nav-link').on('click', (e) => {
+    console.log('clicked');
+    $('#gifContainer').html('');
+    getGifs(e.target.text);
+})
+
+
+$('form').on('submit', () => {
+    const topicToAdd = $('#topicToAdd').val();
+    console.log(topicToAdd);
+    let navItem = $('<li>');
+    navItem.addClass('nav-item');
+
+    const navLink = $('<a>');
+    navLink.addClass('nav-link');
+    navLink.attr('href', '#');
+    navLink.text(topicToAdd);
+    navItem.append(navLink);
+
+    $('#navBar').append(navItem);
+});
