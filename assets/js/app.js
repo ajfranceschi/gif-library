bc72769b6821bdfb44b7fc8b94be667c9f84789f
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-07-20 01:23:18 -0400

    implemented click functionality on gifs, when clicked they start/stop the animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,3 @@
-var queriesArray = []
 
 const createQueryURL = (queryItem) => {
     return "https://api.giphy.com/v1/gifs/search?api_key=GKwA57i0nyuYd3slVSsZEWet5HMrfuD3&q=" + queryItem + "&limit=10&offset=0&rating=PG-13&lang=en";
@@ -14,14 +13,19 @@ const getGifs = (queryItem) => {
         .then(res => {
             for (let gifObject of res.data) {
                 var imgElement = $('<img>');
-                imgElement.attr('src', gifObject.images.downsized.url);
-                imgElement.addClass('col-lg-3 col-md-4 my-3 rounded');
+                imgElement.attr('src', gifObject.images.downsized_still.url);
+                imgElement.attr('data-animate', gifObject.images.downsized.url);
+                imgElement.attr('data-still', gifObject.images.downsized_still.url);
+                imgElement.attr('data-state', 'still');
+                imgElement.addClass('col-lg-3 col-md-4 my-3 rounded gifImg');
                 $('#gifContainer').append(imgElement);
 
             }
 
         });
 }
+
+getGifs('Star Wars');
 
 $('#navBar').on('click', '.nav-link', (e) => {
     $('#gifContainer').html('');
@@ -44,3 +48,19 @@ $('form').on('submit', (e) => {
 
     $('#navBar').append(navItem);
 });
+
+$(document.body).on('click', 'img', function() {
+    let dataState = $(this).attr('data-state');
+    let dataAnimated = $(this).attr('data-animate');
+    let dataStill = $(this).attr('data-still');
+
+    if (dataState === 'still') {
+        $(this).attr('src', dataAnimated);
+        $(this).attr('data-state', 'animate');
+    } else {
+        $(this).attr('src', dataStill);
+        $(this).attr('data-state', 'still');
+
+    }
+})
+
